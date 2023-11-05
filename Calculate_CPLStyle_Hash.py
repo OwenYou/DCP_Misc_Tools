95405d1137b5620dd1060bf8879f5fc2f60d37f4
You Ziyuan <hikaridragon0216@gmail.com>
2023-11-06 02:56:28 +0800

    Update Calculate_CPLStyle_Hash.py
    added english annotation

--- Calculate_CPLStyle_Hash.py
+++ Calculate_CPLStyle_Hash.py
@@ -6,6 +6,7 @@ import os
 from math import ceil
 
 # 输入数据
+# Input Data Path
 audio = r'E:\audio.mxf'
 video = r"E:\video.mxf"
 
@@ -13,13 +14,19 @@ def calculate_dcp_style_hash(filepath, bar = None):
     file_size = os.path.getsize(filepath)
     with open(filepath, "rb") as file:
         # 创建一个SHA-1哈希对象
+        # Create a SHA-1 object.
         sha1_hash = hashlib.sha1()
         # 逐块更新哈希对象以处理文件内容
-        chunk_size = 262144  # 每次读取的字节数, 现在是256KB, 这个数提高到这个值对处理速度提升很大
-        total_work = ceil(file_size/chunk_size)  # 总chunk量
+        # Update the hash object per file chunk.
+        chunk_size = 262144  
+        # ↑每次读取的字节数, 现在是256KB, 这个数提高到这个值对处理速度提升很大
+        # Bytes per chunk, 256KB is optimized for performance.
+        total_work = ceil(file_size/chunk_size)  # Total chunk count
         print(total_work)
         # progress = 0
-        if bar:  # 如果传入progress.bar的Bar对象, 就处理进度条信息
+        if bar:  
+            # 如果传入progress.bar的Bar对象, 就处理进度条信息
+            # if a progress.bar Bar class is present, process info for it.
             bar.max = total_work
             while True:
                 data = file.read(chunk_size)
@@ -28,7 +35,9 @@ def calculate_dcp_style_hash(filepath, bar = None):
                 sha1_hash.update(data)
                 bar.next()
             bar.finish()
-        else:  # 不管progress.bar, 只计算哈希
+        else:  
+            # 不管progress.bar, 只计算哈希
+            # no progress.bar, calculate hash only
             while True:
                 data = file.read(chunk_size)
                 if not data:
@@ -36,10 +45,12 @@ def calculate_dcp_style_hash(filepath, bar = None):
                 sha1_hash.update(data)
 
     # 计算SHA-1哈希HEX值
+    # calculate SHA-1 Hash Hex
     sha1_hex_digest = sha1_hash.hexdigest()
     # print("SHA-1哈希值:", sha1_hex_digest)
     binary_data = bytes.fromhex(sha1_hex_digest)
     # 使用base64进行编码
+    # encode with base64
     base64_encoded = base64.b64encode(binary_data).decode('utf-8')
     # print(base64_encoded)
     return base64_encoded
